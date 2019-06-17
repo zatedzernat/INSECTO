b2bc2f807e4323fcd0fd3f9017dfa7932688a1f5
BILL-ROG <bccbew164@gmail.com>
2019-06-17 22:28:19 +0700

    add navbar to master.blade

--- INSECTO_APP/app/Http/Controllers/RoomController.php
+++ INSECTO_APP/app/Http/Controllers/RoomController.php
@@ -14,7 +14,7 @@ class RoomController extends Controller
      */
     public function index()
     {
-        //
+        return view('room.index');
     }
 
     /**
---
+++ INSECTO_APP/resources/views/room/index.blade.php
@@ -0,0 +1,11 @@
+@extends('master')
+
+@section('title')
+Room ADD
+@endsection
+
+@section('content')
+<div align="center">
+    <h3>Room ALL</h3>
+</div>
+@endsection
