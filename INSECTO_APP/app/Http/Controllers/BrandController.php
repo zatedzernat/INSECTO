822f7a31354bc3442ac777c87ccd400463f48466
Hongpcrw <patcharawan.hong@mail.kmutt.ac.th>
2020-02-24 18:25:54 +0700

    test

--- INSECTO_APP/app/Http/Controllers/BrandController.php
+++ INSECTO_APP/app/Http/Controllers/BrandController.php
@@ -113,7 +113,6 @@ class BrandController extends Controller
     public function hongbrand()
     {
         $data = $this->brand->findByCancelFlag('N');
-        //tiny
         // return response()->json([
         //     'brand' => $data
         // ]);
