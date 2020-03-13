d3625bd40efe4b39db011cdd7c9cb25f4f19a73d
zatedzernat <bccbew164@gmail.com>
2020-03-13 13:59:09 +0700

    [ADD] noti_prob seeder for example

--- INSECTO_APP/database/seeds/Notification_ProblemsTableSeeder.php
+++ INSECTO_APP/database/seeds/Notification_ProblemsTableSeeder.php
@@ -12,6 +12,16 @@ class Notification_ProblemsTableSeeder extends Seeder
      */
     public function run()
     {
-        //not yet (just leave it)
+        DB::table('notification__problems')->insert([
+            'item_id' => 1,
+            'status_id' => 1,
+            'problem_des_id' => 1,
+            'problem_description' => 'จอไม่ติด',
+            'sender_ip' => '127.0.0.1',
+            'cancel_flag' => 'N',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+            'update_by' => 'test'
+        ]);
     }
 }
