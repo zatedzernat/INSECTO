88ce26368083831266443f3de8ae1fcc677f9fdd
zatedzernat <bccbew164@gmail.com>
2020-03-22 17:40:58 +0700

    [REMOVE] send problem link

--- INSECTO_APP/resources/views/master.blade.php
+++ INSECTO_APP/resources/views/master.blade.php
@@ -50,12 +50,6 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav ml-auto">
-                    {{-- <li class="nav-item">
-                        <a class="nav-link" href="{{url('/')}}">Home <span class="sr-only">(current)</span></a>
-                    </li> --}}
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{'/send-problem'}}">Send Problem</a>
-                    </li>
                     <!-- Authentication Links -->
                     @guest
                     <li class="nav-item">
