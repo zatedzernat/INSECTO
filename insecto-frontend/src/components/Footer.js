d410dd37a7af411bd3df46e9abce8ed1f18126c5
zatedzernat <bccbew164@gmail.com>
2020-04-21 22:32:17 +0700

    [ADD] adminLTE-3.0.4 themes then add main components

---
+++ insecto-frontend/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    //   footer from adminLTE-3.0.4 - indexedDB.html
+    <footer className="main-footer">
+      <strong>
+        Copyright © 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.
+      </strong>
+      All rights reserved.
+      <div className="float-right d-none d-sm-inline-block">
+        <b>Version</b> 3.0.4
+      </div>
+    </footer>
+  );
+}
