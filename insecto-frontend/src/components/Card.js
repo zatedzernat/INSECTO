60d039b146f73060b69dc4e7f7698044841db4bb
hongpcrw <patcharawan.hong@mail.kmutt.ac.th>
2020-04-24 00:14:21 +0700

    [ADD] bug photo, Card component

---
+++ insecto-frontend/src/components/Card.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Card(props) {
+  return (
+    <div className="card">
+      <div className="card-header">
+        <h3 className="card-title">{props.title}</h3>
+        <div className="card-tools">
+          {/* Buttons, labels, and many other things can be placed here! */}
+          {/* Here is a label for example */}
+          <span className="badge badge-primary">{props.badge}</span>
+        </div>
+        {/* /.card-tools */}
+      </div>
+      {/* /.card-header */}
+      <div className="card-body ">{props.body}</div>
+      {/* /.card-body */}
+      <div className="card-footer">{props.footer}</div>
+      {/* /.card-footer */}
+    </div>
+    // {/* /.card */}
+  );
+}
