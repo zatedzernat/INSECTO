d87e1aadbcd6fb46f44d9b806ef8be525c241c3e
tinypt <PjungSs@hotmail.co.th>
2020-05-19 05:20:53 +0700

    [EDIT] update nav

--- insecto-frontend/src/views/mobile/Header.js
+++ insecto-frontend/src/views/mobile/Header.js
@@ -1,19 +1,23 @@
 import React from "react";
+import { Navbar, Row, Col } from "react-bootstrap";
 
 export default function Header() {
   return (
-    //   navbar from adminLTE-3.0.4 - indexedDB.html
-    <nav className="navbar navbar-expand navbar-white navbar-light">
-      <div className="row">
+      <Row>
+        <Col>
+        <Navbar className="border-bottom">
           <a href="/" className="brand-link text-dark">
             <img
               src="/images/bug.png"
               alt="insecto logo"
               className="brand-image img-circle elevation-3"
             />
-            <span className="brand-text font-weight-dark">INSECTO</span>
+            <span className="font-weight-dark">
+              INSECTO
+            </span>
           </a>
-      </div>
-    </nav>
+        </Navbar>
+        </Col>
+      </Row>
   );
 }
