9051c65cd00c0ef12fb0e1ed205836973f85fa07
hongpcrw <patcharawan.hong@mail.kmutt.ac.th>
2020-05-18 16:00:37 +0700

    [EDIT] show data, data table, change status

--- insecto-frontend/src/views/NotificationProblems.js
+++ insecto-frontend/src/views/NotificationProblems.js
@@ -1,93 +1,449 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Content from "../components/Content";
 import Card from "../components/Card";
-import { Table } from "react-bootstrap";
 import _ from "lodash";
-import { Button, DropdownButton, ButtonGroup, Dropdown } from "react-bootstrap";
+import axios from "axios";
+import { Button, DropdownButton, Dropdown, Alert } from "react-bootstrap";
+import FormModal from "../components/FormModal";
+import DataTable from "react-data-table-component";
+import moment from "moment";
+
 export default function NotificationProblems() {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [notiProblem, setNotiProblem] = useState({});
+  const [modalShowDetail, setModalShowDetail] = useState(false);
+  const [modalShowHDC, setModalShowHDC] = useState(false);
+  const [modalShowNote, setModalShowNote] = useState(false);
+  const [modalConfirm, setModalConfirm] = useState(false);
+  const [status, setStatus] = useState({});
+  const [isError, setIsError] = useState({
+    error: false,
+    message: "",
+  });
+  const [isSuccess, setIsSuccess] = useState({
+    success: false,
+    message: "",
+  });
+  const [lastUpdate, setLastUpdate] = useState(0);
+
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const res = await axios.get(
+        `${process.env.REACT_APP_API_URL}noti_problems`
+      );
+      setData(res.data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(JSON.stringify(error.response.data.errors));
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [lastUpdate]);
+
+  const handleStatus = async (next_status_id, event) => {
+    if (next_status_id === 2) {
+      setStatus({
+        next_status_id: 2,
+      });
+      setModalShowHDC(true);
+    } else if (
+      next_status_id === 3 ||
+      next_status_id === 4 ||
+      next_status_id === 5
+    ) {
+      switch (next_status_id) {
+        case 3:
+          setStatus({
+            status_name: "on hold",
+            next_status_id: 3,
+          });
+          setModalConfirm(true);
+          break;
+        case 4:
+          setStatus({
+            status_name: "queue",
+            next_status_id: 4,
+          });
+          setModalConfirm(true);
+          break;
+        case 5:
+          setStatus({
+            status_name: "in progress",
+            next_status_id: 5,
+          });
+          setModalConfirm(true);
+          break;
+        default:
+          break;
+      }
+    } else if (next_status_id === 8) {
+      setStatus({
+        next_status_id: 8,
+      });
+      setModalShowNote(true);
+    } else if (next_status_id === 7) {
+      setStatus({
+        next_status_id: 7,
+      });
+      setModalShowHDC(true);
+    }
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setModalShowHDC(false);
+    setModalShowNote(false);
+    setModalConfirm(false);
+    try {
+      const res = await axios.put(
+        `${process.env.REACT_APP_API_URL}noti_problems/${notiProblem.noti_id}`,
+        status
+      );
+      if (res.data.errors) {
+        setIsError({
+          error: true,
+          message: res.data.errors,
+        });
+      } else {
+        setLastUpdate(res.data.time);
+        setIsSuccess({
+          success: true,
+          message: res.data.success,
+        });
+      }
+    } catch (error) {
+      if (error.response.status === 422) {
+        let mess1 = error.response.data.errors.next_status_id
+          ? error.response.data.errors.next_status_id
+          : "";
+        let mess2 = error.response.data.errors.help_desk_code
+          ? error.response.data.errors.help_desk_code
+          : "";
+        let mess3 = error.response.data.errors.note
+          ? error.response.data.errors.note
+          : "";
+        setIsError({
+          error: true,
+          message: mess1 + " " + mess2 + " " + mess3,
+        });
+      }
+    }
+  };
+
+  const showNextStatus = (row) => {
+    let next_status;
+    switch (row.status_id) {
+      case 1:
+        next_status = [{ status_id: 2, status_name: "open" }];
+        break;
+      case 2:
+        next_status = [
+          { status_id: 3, status_name: "on hold" },
+          { status_id: 4, status_name: "queue" },
+          { status_id: 5, status_name: "in progress" },
+        ];
+        break;
+      case 3:
+        next_status = [
+          { status_id: 4, status_name: "queue" },
+          { status_id: 5, status_name: "in progress" },
+          { status_id: 8, status_name: "resolved" },
+        ];
+        break;
+      case 4:
+        next_status = [
+          { status_id: 3, status_name: "on hold" },
+          { status_id: 5, status_name: "in progress" },
+        ];
+        break;
+      case 5:
+        next_status = [
+          { status_id: 3, status_name: "on hold" },
+          { status_id: 8, status_name: "resolved" },
+        ];
+        break;
+      case 7:
+        next_status = [
+          { status_id: 3, status_name: "on hold" },
+          { status_id: 4, status_name: "queue" },
+          { status_id: 5, status_name: "in progress" },
+        ];
+        break;
+      case 8:
+        next_status = [{ status_id: 7, status_name: "reopen" }];
+        break;
+      default:
+        break;
+    }
+
+    return (
+      <DropdownButton
+        title={row.status.status_name}
+        id="bg-nested-dropdown-status"
+        size="sm"
+        variant="warning"
+      >
+        {_.map(next_status, (status) => (
+          <Dropdown.Item
+            key={status.status_id}
+            eventKey={status.status_id}
+            onSelect={(eventKey, event) => {
+              setNotiProblem(row);
+              handleStatus(parseInt(eventKey), event);
+            }}
+          >
+            {status.status_name}
+          </Dropdown.Item>
+        ))}
+      </DropdownButton>
+    );
+  };
+
+  const styles = {
+    container: { color: "red" },
+  };
+
+  const notiProblemTable = (data) => {
+    const columns = [
+      {
+        name: "#",
+        selector: "noti_id",
+        sortable: true,
+      },
+      {
+        name: "Item Code",
+        selector: "item.item_code",
+        sortable: true,
+      },
+      {
+        name: "Item Name",
+        selector: "item.item_name",
+        sortable: true,
+      },
+      {
+        name: "Problem Decription",
+        selector: "problem_description",
+        sortable: true,
+      },
+      {
+        name: "Room Code",
+        selector: "item.room.room_code",
+        sortable: true,
+      },
+      {
+        name: "Status",
+        sortable: true,
+        button: true,
+        allowOverflow: true, //for dropdown
+        cell: (row) => showNextStatus(row),
+      },
+      {
+        name: "Created At",
+        selector: "created_at",
+        sortable: true,
+        format: (r) => moment(r.created_at).format("D/M/YYYY - HH:mm:ss"),
+      },
+      {
+        name: "Updated At",
+        selector: "updated_at",
+        sortable: true,
+        format: (r) => moment(r.updated_at).format("D/M/YYYY - HH:mm:ss"),
+      },
+      {
+        name: "User",
+        selector: "user.name",
+        sortable: true,
+      },
+      {
+        name: "Detail",
+        cell: (row) => (
+          <Button
+            variant="link"
+            onClick={() => {
+              setModalShowDetail(true);
+              setNotiProblem(row);
+            }}
+          >
+            Detail
+          </Button>
+        ),
+        button: true,
+      },
+    ];
+
+    return (
+      <DataTable
+        columns={columns}
+        data={data.noti_problems}
+        striped
+        responsive
+        noHeader
+        selectableRowsHighlight
+        highlightOnHover
+        pagination
+      />
+    );
+  };
+
   return (
     <Content
       content={
-        <Card
-          title={
-            <div>
-              <h2>Notification Problems</h2>
-              <h6>รายการการแจ้งปัญหาทั้งหมด</h6>
-            </div>
-          }
-          badge={
-            <div>
-              <Button variant="info">Add</Button>
-              &emsp;
-              <Button variant="danger">Delete</Button>
-            </div>
-          }
-          body={notiProblemTable()}
-        />
+        <>
+          {isError.error && (
+            <Alert
+              variant="danger"
+              onClose={() => setIsError(false)}
+              dismissible
+            >
+              {isError.message}
+            </Alert>
+          )}
+          {isSuccess.success && (
+            <Alert
+              variant="success"
+              onClose={() => setIsSuccess(false)}
+              dismissible
+            >
+              {isSuccess.message}
+            </Alert>
+          )}
+          <Card
+            title={
+              <div>
+                <h2>Notification Problems</h2>
+                <h6>รายการการแจ้งปัญหาทั้งหมด</h6>
+              </div>
+            }
+            body={notiProblemTable(data)}
+            loading={isLoading ? "overlay" : ""}
+          />
+
+          <FormModal
+            show={modalShowDetail}
+            onHide={() => setModalShowDetail(false)}
+            title="Detail"
+            body={
+              <div className="form-group row">
+                <label className="col-sm-6 col-form-label">Created At: </label>
+                <div className="col-sm-6">{notiProblem.created_at}</div>
+                <label className="col-sm-6 col-form-label">
+                  Problem Description ID:{" "}
+                </label>
+                <div className="col-sm-6">{notiProblem.problem_des_id}</div>
+                <label className="col-sm-6 col-form-label">Room Name:</label>
+                <div className="col-sm-6">
+                  {notiProblem.item?.room.room_name}
+                </div>
+                <label className="col-sm-6 col-form-label">Status </label>
+                <div className="col-sm-6">
+                  {notiProblem.status?.status_name}
+                </div>
+                <label className="col-sm-6 col-form-label">HDC: </label>
+                <div className="col-sm-6">
+                  {notiProblem.help_desk_code ?? "-"}
+                </div>
+                <label className="col-sm-6 col-form-label">Note: </label>
+                <div className="col-sm-6">{notiProblem.note ?? "-"}</div>
+              </div>
+            }
+            method="POST"
+            close="Close"
+          />
+
+          <FormModal
+            show={modalShowHDC}
+            onHide={() => setModalShowHDC(false)}
+            title="Enter your help desk code"
+            body={
+              <div className="form-group row">
+                <label className="col-sm-3 col-form-label">
+                  Help Desk Code: <span style={styles.container}>*</span>
+                </label>
+                <div className="col-sm-9">
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="help_desk_code"
+                    value={notiProblem.help_desk_code ?? ""}
+                    onChange={(event) => {
+                      setStatus({
+                        ...status,
+                        help_desk_code: event.target.value,
+                      });
+                      setNotiProblem({
+                        ...notiProblem,
+                        help_desk_code: event.target.value,
+                      });
+                    }}
+                    required
+                    autoFocus
+                  />
+                </div>
+              </div>
+            }
+            method="PUT"
+            onSubmit={handleSubmit}
+            button="Confirm"
+            close="Close"
+          />
+          <FormModal
+            show={modalShowNote}
+            onHide={() => setModalShowNote(false)}
+            title="Enter your note"
+            body={
+              <div className="form-group row">
+                <label className="col-sm-3 col-form-label">
+                  Note: <span style={styles.container}>*</span>
+                </label>
+                <div className="col-sm-9">
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="note"
+                    value={notiProblem.note ?? ""}
+                    onChange={(event) => {
+                      setStatus({
+                        ...status,
+                        note: event.target.value,
+                      });
+                      setNotiProblem({
+                        ...notiProblem,
+                        note: event.target.value,
+                      });
+                    }}
+                    required
+                    autoFocus
+                  />
+                </div>
+              </div>
+            }
+            method="PUT"
+            onSubmit={handleSubmit}
+            button="Confirm"
+            close="Close"
+          />
+          <FormModal
+            show={modalConfirm}
+            onHide={() => setModalConfirm(false)}
+            title="Confirm Change Status"
+            body={
+              <div className="form-group row">
+                <label className="col-sm-12 col-form-label">
+                  Are you sure to change status to " <span style={styles.container}>{status.status_name}</span> " ?
+                </label>
+              </div>
+            }
+            method="PUT"
+            onSubmit={handleSubmit}
+            button="Yes"
+            close="No"
+          />
+        </>
       }
     />
   );
 }
-
-const notiProblemTable = () => {
-  const data = [
-    {
-      item_id: 1,
-      item_code: "123",
-      item_name: "หลอดไฟ",
-      problem_des: "ไฟกระพริบ",
-      room_code: "AAA",
-      status: "waiting button",
-      created_at: "test",
-      updated_at: "test",
-      update_by: "seeder",
-    },
-  ];
-  return (
-    <Table striped hover>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Item Code</th>
-          <th>Item Name</th>
-          <th>Problem Description</th>
-          <th>Room Code</th>
-          <th>Status</th>
-          <th>Created At</th>
-          <th>Updated At</th>
-          <th>Update By</th>
-          <th>Detail</th>
-        </tr>
-      </thead>
-      <tbody>
-        {_.map(data, (notiProblem) => (
-          <tr key={notiProblem.item_id}>
-            <td>{notiProblem.item_id}</td>
-            <td>{notiProblem.item_code}</td>
-            <td>{notiProblem.item_name}</td>
-            <td>{notiProblem.problem_des}</td>
-            <td>{notiProblem.room_code}</td>
-            {/* <td>{notiProblem.status}</td> */}
-            <td>
-              <DropdownButton
-                as={ButtonGroup}
-                title="waiting"
-                id="bg-nested-dropdown"
-                size="sm"
-                variant="warning"
-              >
-                <Dropdown.Item eventKey="1">open</Dropdown.Item>
-              </DropdownButton>
-            </td>
-            <td>{notiProblem.created_at}</td>
-            <td>{notiProblem.updated_at}</td>
-            <td>{notiProblem.update_by}</td>
-            <td>
-              <Button variant="link">Detail</Button>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </Table>
-  );
-};
