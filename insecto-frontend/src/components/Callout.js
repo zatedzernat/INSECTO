bd33e306b4a2224fa4e3fa588e5664fcb796bcc7
hongpcrw <patcharawan.hong@mail.kmutt.ac.th>
2020-06-10 16:48:27 +0700

    [ADD] Callout Component

---
+++ insecto-frontend/src/components/Callout.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+
+export default function Callout(props) {
+  return (
+    <div className={props.color}>
+      <Row>
+        <Col xs={7}>
+          <h5>{props.item}</h5>
+        </Col>
+        <Col style={{ textAlign: "right" }}>
+          <h6>{props.status}</h6>
+        </Col>
+      </Row>
+      <Row>
+        <Col>{props.problem}</Col>
+        <Col style={{ textAlign: "right" }}><h6>{props.time}</h6></Col>
+      </Row>
+    </div>
+  );
+}
