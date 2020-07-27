77ce0cd6c6bd4a8c9c4c7366baa7fa191a6cd962
tinypt <PjungSs@hotmail.co.th>
2020-07-27 16:59:57 +0700

    [EDIT] update ui tracking page and connect api

---
+++ insecto-frontend/src/components/CardTracking.js
@@ -0,0 +1,115 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+export default function Card(props) {
+  const statusBar = () => {
+    var styles;
+    var textStatus = "";
+
+    switch (props.status) {
+      case "resolved":
+        styles = {
+          borderColor: "#93D388",
+          borderWidth: 1,
+          borderStyle: "solid",
+          height: 35,
+          width: 100,
+          backgroundColor: "#43CF2A",
+          borderRadius: 5
+        };
+        textStatus = "ดำเนินการเสร็จสิ้น";
+        break;
+
+      case "closed":
+        styles = {
+          borderColor: "#93D388",
+          borderWidth: 1,
+          borderStyle: "solid",
+          height: 35,
+          width: 100,
+          backgroundColor: "#43CF2A",
+          borderRadius: 5
+
+        };
+        textStatus = "ดำเนินการเสร็จสิ้น";
+        break;
+
+      case "in progress":
+        styles = {
+          borderColor: "#A0DCFF",
+          borderWidth: 1,
+          borderStyle: "solid",
+          height: 35,
+          width: 100,
+          backgroundColor: "#2BB0FF",
+          borderRadius: 5
+        };
+        textStatus = "กำลังดำเนินการ";
+        break;
+
+      default:
+        styles = {
+          borderColor: "#FBD4FF",
+          borderWidth: 1,
+          borderStyle: "solid",
+          height: 35,
+          width: 100,
+          backgroundColor: "#DAA6DF",
+          borderRadius: 5
+        };
+        textStatus = "รอการดำเนินการ";
+        break;
+    }
+
+    return (
+      <Container fluid className="mt-1 text-center pt-2" style={styles}>
+        <p style={{ fontSize: 11, color: 'white' }}>{textStatus}</p>
+      </Container>
+    );
+  };
+  return (
+    <Container
+      fluid
+      className="m-0 pt-2 pl-4 pb-2 pr-2"
+      style={{
+        height: 60,
+      }}
+    >
+      <Row>
+        <Col className="col-5">
+          <Row className="pb-0 mb-0" style={{ height: 22 }}>
+            <p style={{ fontSize: 14 }}>
+              {props.itemName.length > 15
+                ? props.itemName.slice(0, 15) + "..."
+                : props.itemName}
+            </p>
+          </Row>
+          <Row>
+            <p style={{ fontSize: 11, color: "#777777" }}>
+              {props.itemProblem.length > 20
+                ? props.itemProblem.slice(0, 20) + "..."
+                : props.itemProblem}
+            </p>
+          </Row>
+        </Col>
+        <Col className="col-1">
+          <Row>
+            <p style={{ fontSize: 11, paddingTop: 3 }}>
+              {props.room.length > 10
+                ? props.room.slice(0, 10) + "..."
+                : props.room}
+            </p>
+          </Row>
+          <Row></Row>
+        </Col>
+        <Col className="col-5">{statusBar()}</Col>
+        <Col className="col-1 text-center pt-2">
+          <i
+            className="fa fa-angle-right"
+            style={{ color: "#777777", fontSize: 30 }}
+          ></i>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
--- insecto-frontend/src/views/mobile/trackingProblem.js
+++ insecto-frontend/src/views/mobile/trackingProblem.js
@@ -1,20 +1,74 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import _ from "lodash";
+import axios from "axios";
+import Card from "../../components/CardTracking";
 
-export default function trackingProblem() {
+export default function TrackingProblem() {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [notiProblem, setNotiProblem] = useState({});
+  const [status, setStatus] = useState({});
+
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const res = await axios.get(
+        `${process.env.REACT_APP_API_URL}noti_problems`
+      );
+      setData(res.data.noti_problems);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(JSON.stringify(error.response.data.errors));
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const statusBar = () => {
+    const arr = [];
+    // for (let key in data) {
+      arr.push(
+     data.map((item) => (
+      <Row
+        style={{
+          borderColor: "#E2E2E2",
+          borderBottomWidth: 1,
+          borderBottomStyle: "solid",
+        }}
+      >
+        <Card
+          itemName={item.item.item_code + " " + item.item.item_name}
+          itemProblem={item.problem_description}
+          room={item.item.room.room_name}
+          status={item.status.status_name}
+        />
+      </Row>
+    ))) 
+    return arr;
+  };
+
   return (
     <>
       <Container>
         <Row>
-          <Col>
-            <h1>tracking problem</h1>
+          <Col
+            className="pt-4 pb-2"
+            style={{
+              backgroundColor: "#F0F3F5",
+              borderColor: "#E0E0E0",
+              borderBottomWidth: 3,
+              borderBottomStyle: "solid",
+            }}
+          >
+            <p className="text-center p-0" style={{ fontSize: 20 }}>
+              ระบบติดตามสถานะการซ่อมครุภัณฑ์
+            </p>
           </Col>
         </Row>
-        <Row className="border-bottom">
-          <Col>
-            <h5>ปัญหาที่ถูกส่งแล้ว, ส่งเมื่อไร, status</h5>
-          </Col>
-        </Row>
+        {statusBar()}
       </Container>
     </>
   );
