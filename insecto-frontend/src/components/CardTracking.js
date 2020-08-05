6137b590d8dc6409f69966e78d3e76e47df17b03
tinypt <PjungSs@hotmail.co.th>
2020-08-05 19:50:17 +0700

    [Edit] update ui

--- insecto-frontend/src/components/CardTracking.js
+++ insecto-frontend/src/components/CardTracking.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height,
+  };
+}
+
+function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowDimensions;
+}
+
 export default function Card(props) {
+  const { height, width } = useWindowDimensions();
   const statusBar = () => {
     var styles;
     var font;
@@ -14,12 +40,12 @@ export default function Card(props) {
           borderWidth: 1,
           borderStyle: "solid",
           height: 22,
-          width: 99,
+          width: 100,
           backgroundColor: "#FAFAFA",
-          borderRadius: 5
+          borderRadius: 5,
         };
         textStatus = "ดำเนินการเสร็จสิ้น";
-        font= {color: '#43CF2A', fontSize: 11}
+        font = { color: "#43CF2A", fontSize: 11 };
         break;
 
       case "closed":
@@ -28,13 +54,12 @@ export default function Card(props) {
           borderWidth: 1,
           borderStyle: "solid",
           height: 22,
-          width: 99,
+          width: 100,
           backgroundColor: "#FAFAFA",
-          borderRadius: 5
-
+          borderRadius: 5,
         };
         textStatus = "ดำเนินการเสร็จสิ้น";
-         font= {color: '#43CF2A', fontSize: 11}
+        font = { color: "#43CF2A", fontSize: 11 };
         break;
 
       case "in progress":
@@ -43,31 +68,31 @@ export default function Card(props) {
           borderWidth: 1,
           borderStyle: "solid",
           height: 22,
-          width: 99,
+          width: 90,
           backgroundColor: "#FAFAFA",
-          borderRadius: 5
+          borderRadius: 5,
         };
         textStatus = "กำลังดำเนินการ";
-         font= {color: '#2BB0FF', fontSize: 11}
+        font = { color: "#2BB0FF", fontSize: 11 };
         break;
 
       default:
         styles = {
-          borderColor: "#FBD4FF",
+          borderColor: "rgb(255 148 234)",
           borderWidth: 1,
           borderStyle: "solid",
           height: 22,
-          width: 99,
+          width: 97,
           backgroundColor: "#FAFAFA",
           borderRadius: 5,
         };
         textStatus = "รอการดำเนินการ";
-         font= {color: '#F19EFF', fontSize: 11}
+        font = { color: "rgb(241 79 210)", fontSize: 11 };
         break;
     }
 
     return (
-      <Container fluid className="mt-1 text-center pt-1 ml-3" style={styles}>
+      <Container fluid className="mt-1 text-center pt-1" style={styles}>
         <p style={font}>{textStatus}</p>
       </Container>
     );
@@ -84,25 +109,26 @@ export default function Card(props) {
         <Col className="col-4">
           <Row className="pb-0 mb-0" style={{ height: 22 }}>
             <p style={{ fontSize: 14 }}>
-              {props.itemName.length > 15
-                ? props.itemName.slice(0, 15) + "..."
+              {width <= 700
+                ? props.itemName.length > 14
+                  ? props.itemName.slice(0, 14) + "..."
+                  : props.itemName
                 : props.itemName}
             </p>
           </Row>
           <Row>
             <p style={{ fontSize: 11, color: "#777777" }}>
-              {props.itemProblem.length > 20
-                ? props.itemProblem.slice(0, 20) + "..."
-                : props.itemProblem}
+              {width <= 700
+                ? props.itemProblem.length > 18
+                ? props.itemProblem.slice(0, 18) + "..."
+                : props.itemProblem : props.itemProblem}
             </p>
           </Row>
         </Col>
         <Col className="col-3  mt-2 text-right">
-            <p  style={{ fontSize: 11, paddingTop: 3 }}>
-                {props.room}
-            </p>
+          <p style={{ fontSize: 11, paddingTop: 3 }}>{props.room}</p>
         </Col>
-        <Col className="col-4 mt-2">{statusBar()}</Col>
+        <Col className="col-4 mt-2 text-center">{statusBar()}</Col>
         <Col className="col-1 text-center pt-2">
           <i
             className="fa fa-angle-right"
