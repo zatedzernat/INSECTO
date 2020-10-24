981dffa6a892d27602b848f8e9bffbd06fe24506
zatedzernat <bccbew164@gmail.com>
2020-10-24 17:00:25 +0700

    [ADD] private route and fetch me to get user

---
+++ insecto-frontend/src/components/PrivateRoute.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+import { Route, useHistory } from "react-router-dom";
+import Cookies from "js-cookie";
+import axios from "axios";
+
+const PrivateRoute = ({ component: Component, layout: Layout, ...rest }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState(null);
+  const token = Cookies.get("token");
+  const history = useHistory();
+
+  useEffect(() => {
+    checkUser();
+    fetchme();
+    // eslint-disable-next-line
+  }, []);
+
+  const fetchme = async () => {
+    let data = {
+      token: token,
+    };
+    try {
+      const res = await axios.post(
+        `${process.env.REACT_APP_API_URL}sso/fetchme`,
+        data
+      );
+      setUser(res.data);
+    } catch (error) {
+      console.log(JSON.stringify(error));
+    }
+  };
+
+  const checkUser = () => {
+    if (token) {
+      setIsAuthenticated(true);
+    } else {
+      history.replace({
+        pathname: "/admin",
+        state: {
+          isActive: true,
+        },
+      });
+    }
+  };
+
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        isAuthenticated ? (
+          <Layout user={user} {...props}>
+            <Component user={user} {...props} />
+          </Layout>
+        ) : (
+          <></>
+        )
+      }
+    />
+  );
+};
+
+export default PrivateRoute;
