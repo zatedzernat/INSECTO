cd288f6ebd9837ac75ecc07f93e5541e29e52936
zatedzernat <bccbew164@gmail.com>
2020-10-28 17:13:17 +0700

    [EDIT] user table seeder

--- INSECTO_APP/database/seeds/UsersTableSeeder.php
+++ INSECTO_APP/database/seeds/UsersTableSeeder.php
@@ -20,13 +20,6 @@ class UsersTableSeeder extends Seeder
             'updated_at' => Carbon::now(),
         ]);
         DB::table('users')->insert([
-            'name' => 'wait for log-in',
-            'email' => 'wait@mail.com',
-            'password' => Hash::make(config('app.test_password')),
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
-        ]);
-        DB::table('users')->insert([
             'name' => 'BILL',
             'email' => '60130500055@st.sit.kmutt.ac.th',
             'password' => Hash::make(config('app.test_password')),
