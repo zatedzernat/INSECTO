199190c08e059c4ce4c983ec043cc70328d40bb3
zatedzernat <bccbew164@gmail.com>
2020-10-28 19:32:07 +0700

    [ADD] user create and update request

---
+++ INSECTO_APP/app/Http/Requests/UserUpdateRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'id' => 'required',
+            'name' => 'required',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'id.required' => 'ID is required!',
+            'name.required' => 'Name is required!',
+        ];
+    }
+}
