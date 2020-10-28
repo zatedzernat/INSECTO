a0c4fe692d9d5e17de29c1fe2aeabf544dab864c
zatedzernat <bccbew164@gmail.com>
2020-10-28 18:01:42 +0700

    [ADD] user controller

---
+++ INSECTO_APP/app/Http/Controllers/UserController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Models\User;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+
+    private $user;
+
+    public function __construct()
+    {
+        $this->user = new User();
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = $this->user->getAll();
+        return compact('users');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $user_id)
+    {
+        $id = $request->input('id');
+        $name = $request->input('name');
+        $isSuccess = $this->user->updateUser($id, $name);
+        if ($isSuccess) {
+            $success = 'Update user \'' . $name . '\' success';
+            return  $this->serverResponse(null, $success);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+
+    public function serverResponse($error, $success)
+    {
+        $time = Carbon::now()->format('H:i:s');
+        return response()->json([
+            'errors' => $error,
+            'success' => $success,
+            'time' => $time
+        ]);
+    }
+}
